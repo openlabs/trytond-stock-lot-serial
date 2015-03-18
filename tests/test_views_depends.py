9f098ed35f8b48e0ffdec22213b8da7c5e3d0dac
vishu jain <vishu@vishu-Vostro-3446.(none)>
2015-03-18 17:23:04 +0530

    Fixed flake8 issue #7320

--- tests/test_views_depends.py
+++ tests/test_views_depends.py
@@ -7,13 +7,6 @@
     :copyright: (C) 2013-2014 by Openlabs Technologies & Consulting (P) Limited
     :license: 3-clause BSD, see LICENSE for more details.
 """
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(
-    __file__, '..', '..', '..', '..', '..', 'trytond'
-)))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 import unittest
 
 import trytond.tests.test_tryton
